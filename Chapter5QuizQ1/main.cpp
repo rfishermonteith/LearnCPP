498050b8bf6ed20cf0d05ceeeb67083499bc078e
rfishy1 <richard.carl.fisher@gmail.com>
2016-04-25 19:55:03 +0200

    Add Chapter 5 Quiz Q1

---
+++ Chapter5QuizQ1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "constants.h"
+
+// gets initial height from user and returns it
+double getInitialHeight()
+{
+	std::cout << "Enter the height of the tower in meters: ";
+	double initialHeight;
+	std::cin >> initialHeight;
+	return initialHeight;
+}
+
+// Returns height from ground after "seconds" seconds
+double calculateHeight(double initialHeight, int seconds)
+{
+	// Using formula: [ s = u * t + (a * t^2) / 2 ], here u(initial velocity) = 0
+	double distanceFallen = (myConstants::gravity * seconds * seconds) / 2;
+	double currentHeight = initialHeight - distanceFallen;
+
+	return currentHeight;
+}
+
+// Prints height every second till ball has reached the ground
+void printHeight(double height, int seconds)
+{
+	if (height > 0.0)
+	{
+		std::cout << "At " << seconds << " seconds, the ball is at height:\t" << height <<
+			" meters\n";
+	}
+	else
+		std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
+}
+
+void calculateAndPrintHeight(double initialHeight, int seconds)
+{
+	double height = calculateHeight(initialHeight, seconds);
+	printHeight(height, seconds);
+}
+
+int main()
+{
+	const double initialHeight = getInitialHeight();
+
+    double currentHeight = initialHeight;
+    double currentTime = 0;
+	while (currentHeight > 0)
+	{
+        currentHeight = calculateHeight(initialHeight, currentTime);
+        printHeight(currentHeight, currentTime);
+        ++currentTime;
+	}
+
+	return 0;
+}
